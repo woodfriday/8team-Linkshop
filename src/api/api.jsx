ba9efc6c204c35d908ace0675a4118d768186e02
JEONBYEONGJIN <qudwls5020@naver.com>
2024-12-22 02:01:13 +0900

    Implement search result handling and display no results image

--- src/api/api.jsx
+++ src/api/api.jsx
@@ -1,8 +1,32 @@
 // apis/api.js
-export const getLinkShopList = () => {
-  return fetch("https://linkshop-api.vercel.app/12-8/linkshops");
+export const getLinkShopList = (keyword, orderby, cursor) => {
+  const url = new URL("https://linkshop-api.vercel.app/12-8/linkshops");
+
+  if (keyword) {
+    url.searchParams.append("keyword", keyword);
+  }
+  if (orderby) {
+    url.searchParams.append("orderBy", orderby);
+  }
+  if (cursor) {
+    url.searchParams.append("cursor", cursor);
+  }
+
+  return fetch(url.href);
 };
 
-export const getLinkshopDetail = async (linkShopId) => {
-  return fetch(`https://linkshop-api.vercel.app/12-8/linkshops/${linkShopId}`);
+export const getLinkShopLike = (linkShopId, isLiked) => {
+  const method = isLiked ? "POST" : "DELETE";
+
+  return fetch(
+    `https://linkshop-api.vercel.app/12-8/linkshops/${linkShopId}/like`,
+    {
+      method: method,
+      headers: {
+        Accept: "application/json",
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify({ isLiked: isLiked }),
+    }
+  );
 };
